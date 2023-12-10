9a15477b6f2d624a269523097f9ced9eb643956e
Dolly62 <kambojdolly798@gmail.com>
2023-12-10 20:14:23 +0530

    Responsive Website using ReactJS & Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import ImageSlider from "./components/ImageSlider";
+import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
+import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Offer/>
+      <Plan/>
+      <Rooms/>
+      <ImageSlider/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
